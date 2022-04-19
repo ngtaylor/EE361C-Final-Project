022f261355fb08dbc6a3d10b59c6f41009aa0523
JamesRJanecka <jamesjanecka@utexas.edu>
2022-04-19 13:11:09 -0500

    CoarseLockCuckoo tested and working
    also added and tested NormalCuckoo

--- src/cuckoo_hashing/CuckooTest.java
+++ src/cuckoo_hashing/CuckooTest.java
@@ -1,5 +1,126 @@
+/* CuckooTest.java
+ * EE361C Final Project
+ */
+
 package cuckoo_hashing;
 
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.concurrent.*;
+
 public class CuckooTest {
+
+//    public class TestLockThread implements Runnable{
+//        LockCuckoo hash;
+//        public TestLockThread(LockCuckoo hash) {
+//            this.hash = hash;
+//        }
+//        public void run() {
+//        	/* following array doesn't have any cycles and hence all keys will be inserted without any rehashing */
+//        	int keys[] = {20, 50, 53, 75, 100, 67, 105, 3, 36, 39};
+//        	int n = keys.length;
+//        	// start with placing every key at its position in
+//        	// the first hash table according to first hash
+//        	// function
+//        	for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
+//        		hash.place(keys[i], 0, cnt, n);
+//        }
+//    }
+
+    public class TestCoarseLockThread implements Runnable{
+        CoarseLockCuckoo hash;
+        public TestCoarseLockThread(CoarseLockCuckoo hash) {
+            this.hash = hash;
+        }
+        
+        public void run() {
+    		/* following array doesn't have any cycles and hence all keys will be inserted without any rehashing */
+    		int keys[] = {20, 50, 53, 75, 100, 67, 105, 3, 36, 39};
+    		int n = keys.length;
+    		// start with placing every key at its position in
+    		// the first hash table according to first hash
+    		// function
+    		for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
+    			hash.place(keys[i], 0, cnt, n);
+        }
+    }
+
+//    public class TestFreeThread implements Runnable{
+//        LockFreeCuckoo hash;
+//        public TestFreeThread(LockFreeCuckoo hash) {
+//            this.hash = hash;
+//        }
+//        public void run() {
+//    		/* following array doesn't have any cycles and hence all keys will be inserted without any rehashing */
+//    		int keys[] = {20, 50, 53, 75, 100, 67, 105, 3, 36, 39};
+//    		int n = keys.length;
+//    		// start with placing every key at its position in
+//    		// the first hash table according to first hash
+//    		// function
+//    		for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
+//    			hash.place(keys[i], 0, cnt, n);
+//        }
+//    }
+
+//    @Test
+//    public void testLockCuckoo() throws ExecutionException, InterruptedException {
+//
+//    }
+
+    @Test
+    public void testCoarseLockCuckoo() throws ExecutionException, InterruptedException {
+        CoarseLockCuckoo hash = new CoarseLockCuckoo();
+        int numThreads = 8;
+        Thread[] threads = new Thread[numThreads];
+        for(int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(new TestCoarseLockThread(hash));
+            threads[i].start();
+        }
+        // finish threads
+        for(Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+		// print the final table
+		hash.printTable();
+		int count = hash.itemCount();
+		Assert.assertTrue("Item count is correct", count == 10);
+    }
+
+//    @Test
+//    public void testLockFreeCuckoo() {
+//
+//    }
+
+    //This tests a normal chain hash table that does not involve concurrent algorithms
+    @Test
+    public void testNormalChain(){
+        NormalCuckoo hash = new NormalCuckoo();
+		/* following array doesn't have any cycles and hence all keys will be inserted without any rehashing */
+		int keys[] = {20, 50, 53, 75, 100, 67, 105, 3, 36, 39};
+		int n = keys.length;
+		// start with placing every key at its position in the first hash table according to first hash function
+		for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
+			hash.place(keys[i], 0, cnt, n);
+		// print the final table
+		hash.printTable();
+		int count = hash.itemCount();
+		Assert.assertTrue("Item count is correct", count == n);		
+
+//        NormalCuckoo hash2 = new NormalCuckoo();
+//		/* following array has a cycle and hence we will have to rehash to position every key */
+//		int keys_2[] = {20, 50, 53, 75, 100, 67, 105, 3, 36, 39, 6};
+//		int m = keys_2.length;
+//		// start with placing every key at its position in the first hash table according to first hash function
+//		for (int i = 0, cnt = 0; i < m; i++, cnt = 0)
+//			hash2.place(keys_2[i], 0, cnt, m);
+//		// print the final table
+//		hash2.printTable();
+//		int count2 = hash.itemCount();
+//		Assert.assertTrue("Item count is correct", count2 == m-1);
+    }
 
 }
---
+++ src/cuckoo_hashing/NormalCuckoo.java
@@ -0,0 +1,145 @@
+package cuckoo_hashing;
+
+public class NormalCuckoo {
+	
+	//upper bound on number of elements in our set
+	static int MAXN = 11;
+
+	//choices for position
+	static int ver = 2;
+
+	//Auxiliary space bounded by a small multiple
+	//of MAXN, minimizing wastage
+	int [][]hashtable;
+
+	//Array to store possible positions for a key
+	int []pos;
+	
+	/* function to fill hash table with dummy value
+	 * dummy value: INT_MIN
+	 * number of hashtables: ver */
+	void initTable() {
+		for (int j = 0; j < MAXN; j++)
+			for (int i = 0; i < ver; i++)
+				hashtable[i][j] = Integer.MIN_VALUE;
+	}
+	
+	public NormalCuckoo() {
+		hashtable = new int[ver][MAXN];
+		pos = new int[ver];
+		initTable();
+	}
+	
+	/* return hashed value for a key
+	 * function: ID of hash function according to which key has to hashed
+	 * key: item to be hashed */
+	int hash(int function, int key) {
+		switch (function) {
+			case 1: return key % MAXN;
+			case 2: return (key / MAXN) % MAXN;
+		}
+		return Integer.MIN_VALUE;
+	}
+	
+
+	/* function to place a key in one of its possible positions
+	 * tableID: table in which key has to be placed, also equal to function according to which key must be hashed
+	 * cnt: number of times function has already been called in order to place the first input key
+	 * n: maximum number of times function can be recursively called before stopping and declaring presence of cycle */
+	void place(int key, int tableID, int cnt, int n) {
+		/* if function has been recursively called max number of times, stop and declare cycle. Rehash. */
+		if (cnt == n) {
+			System.out.printf("%d unpositioned\n", key);
+			System.out.printf("Cycle present. REHASH.\n");
+			return;
+		}
+
+		/* calculate and store possible positions for the key.
+		 * check if key already present at any of the positions. 
+		 * If YES, return. */
+		for (int i = 0; i < ver; i++) {
+			pos[i] = hash(i + 1, key);
+			if (hashtable[i][pos[i]] == key)
+				return;
+		}
+
+		/* check if another key is already present at the position for the new key in the table
+		 * If YES: place the new key in its position
+		 * and place the older key in an alternate position for it in the next table */
+		if (hashtable[tableID][pos[tableID]] != Integer.MIN_VALUE) {
+			int dis = hashtable[tableID][pos[tableID]];
+			hashtable[tableID][pos[tableID]] = key;
+			place(dis, (tableID + 1) % ver, cnt + 1, n);
+		}
+		else // else: place the new key in its position
+			hashtable[tableID][pos[tableID]] = key;
+	}
+
+	/* function to print hash table contents */
+	void printTable() {
+		System.out.printf("Final hash tables:\n");
+
+		for (int i = 0; i < ver; i++, System.out.printf("\n"))
+			for (int j = 0; j < MAXN; j++)
+				if(hashtable[i][j] == Integer.MIN_VALUE)
+					System.out.printf("- ");
+				else
+					System.out.printf("%d ", hashtable[i][j]);
+
+		System.out.printf("\n");
+	}
+	
+	/* function to return the count of items in the hash table */
+	int itemCount() {
+		int count = 0;
+		
+		for (int i = 0; i < ver; i++) {
+			for (int j = 0; j < MAXN; j++) {
+				if(hashtable[i][j] == Integer.MIN_VALUE) {
+					// ignore initial values
+				} else {
+					count++; 
+				}	
+			}
+		}
+		return count;
+	}
+
+	/* function for Cuckoo-hashing keys
+	 * keys[]: input array of keys
+	 * n: size of input array */
+//	static void cuckoo(int keys[], int n) {
+//		// initialize hash tables to a dummy value
+//		// (INT-MIN) indicating empty position
+//		initTable();
+//
+//		// start with placing every key at its position in
+//		// the first hash table according to first hash
+//		// function
+//		for (int i = 0, cnt = 0; i < n; i++, cnt = 0)
+//			place(keys[i], 0, cnt, n);
+//
+//		// print the final hash tables
+//		printTable();
+//	}
+
+	//Driver Code
+//	public static void main(String[] args)
+//	{
+//		/* following array doesn't have any cycles and hence all keys will be inserted without any rehashing */
+//		int keys_1[] = {20, 50, 53, 75, 100,
+//				67, 105, 3, 36, 39};
+//
+//		int n = keys_1.length;
+//
+//		cuckoo(keys_1, n);
+//
+//		/* following array has a cycle and hence we will have to rehash to position every key */
+//		int keys_2[] = {20, 50, 53, 75, 100,
+//				67, 105, 3, 36, 39, 6};
+//
+//		int m = keys_2.length;
+//
+//		cuckoo(keys_2, m);
+//	}
+}
